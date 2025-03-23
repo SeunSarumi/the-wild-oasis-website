def2536d6d0ccb216d22bc104c9ca735f4b1c30d
SeunSarumi <seunsarumi@gmail.com>
2025-03-23 16:08:52 +0000

    project organisation and import alias uses

--- app/cabins/page.jsx
+++ app/cabins/page.jsx
@@ -1,22 +1,9 @@
-import Counter from "../components/counter.jsx";
-import Navigation from "../components/Navigation.js";
+import Counter from "@/app/_components/counter.jsx";
 
-export default async function Page() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users");
-
-  const data = await res.json();
-
+export default function Page() {
   return (
     <div>
       <h1>Cabins page</h1>
-
-      <ul>
-        {data.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
-
-      <Counter users={data} />
     </div>
   );
 }

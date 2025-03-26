91c772152dc2369e48f97698fe89964765ba2db7
SeunSarumi <seunsarumi@gmail.com>
2025-03-26 18:34:03 +0000

    improved the navigation and root element

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,9 +1,10 @@
 import Logo from "@/app/_components/Logo";
-import Navigation from "@/app/_components/Navigation";
 
 import "@/app/_styles/globals.css";
 
 import { Josefin_Sans } from "next/font/google";
+import Navigation from "./_components/Navigation";
+import Header from "./_components/Header";
 
 const josefin = Josefin_Sans({
   subsets: ["latin"],
@@ -23,15 +24,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={` ${josefin.className} min-h-screen bg-primary-950 text-primary-100`}
+        className={` ${josefin.className}  antialiased min-h-screen bg-primary-950 text-primary-100 flex flex-col`}
       >
-        <header>
-          <Logo />
-        </header>
-        <Navigation />
-        <main>{children}</main>
-
-        <footer>Copyright by The Wild Oasis</footer>
+        <Header />
+        <div className="flex-1 px-8 py-12 1">
+          <main className="mx-auto max-w-7xl">{children}</main>
+        </div>
       </body>
     </html>
   );

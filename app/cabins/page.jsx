3cd789e869535036a7fa2edf1ac404c01122af66
SeunSarumi <seunsarumi@gmail.com>
2025-04-15 01:30:23 +0100

    refactored fetching into CabinList and added suspense

--- app/cabins/page.jsx
+++ app/cabins/page.jsx
@@ -1,17 +1,14 @@
 import Counter from "@/app/_components/counter.jsx";
-import CabinCard from "../_components/CabinCard";
-import { getCabins } from "../_lib/data-service";
+
+import CabinList from "./CabinList";
+import { Suspense } from "react";
+import Spinner from "../_components/Spinner";
 
 export const metadata = {
   title: "Cabins",
 };
 
-export default async function Page() {
-  console.log("Starting....");
-
-  const cabins = await getCabins();
-  console.log(cabins);
-
+export default function Page() {
   return (
     <div>
       <h1 className="mb-5 text-4xl font-medium text-accent-400">
@@ -26,13 +23,9 @@ export default async function Page() {
         Welcome to paradise.
       </p>
 
-      {cabins.length > 0 && (
-        <div className="grid gap-8 sm:grid-cols-1 md:grid-cols-2 lg:gap-12 xl:gap-14">
-          {cabins.map((cabin) => (
-            <CabinCard cabin={cabin} key={cabin.id} />
-          ))}
-        </div>
-      )}
+      <Suspense fallback={<Spinner />}>
+        <CabinList />
+      </Suspense>
     </div>
   );
 }
